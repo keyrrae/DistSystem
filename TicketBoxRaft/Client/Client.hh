fb6e90f94ac942a2e0e02c82fb768ed7610390ca
keyrrae <xuan-wang@outlook.com>
2017-02-15 13:58:08 -0800

    basic client cli

--- TicketBoxRaft/Client/Client.hh
+++ TicketBoxRaft/Client/Client.hh
@@ -1,26 +1,33 @@
 <?hh
 
 class Config {
-  public string $serverAddress;
-  public int $maxAttempt;
+  public string $server_address;
+  public int $max_attempt;
   public int $timeout;
+  public string $json_str;
 
   public function __construct(string $confFilename): void {
-    $jsonStr = file_get_contents($confFilename);
-    $json = json_decode($jsonStr, true);
-    $this->serverAddress = $json['address'];
-    $this->maxAttempt = $json['max_attempts'];
+    $this->json_str = file_get_contents($confFilename);
+    $json = json_decode($this->json_str, true);
+    $this->server_address = $json['address'];
+    $this->max_attempt = $json['max_attempts'];
     $this->timeout = $json['timeout'];
+  }
 
+  public function printStats(): void {
+    echo $this->json_str;
   }
 }
 
 class Client {
 
   private Config $config;
+  private bool $waiting_for_input = false;
 
   public function printUsage(): void {
-    echo "b <Number of tickets>\n";
+    echo "b                <Number of tickets>\n";
+    echo "q/quit/e/exit    quit\n";
+
   }
 
   public function __construct(string $confFilename) {
@@ -29,6 +36,34 @@ class Client {
 
   public function run(): void {
     $this->printUsage();
+    for (; ; ) {
+      $this->waiting_for_input = true;
+      $cmd = readline("> ");
+      dispatch($cmd);
+    }
+  }
+
+  private function dispatch(string $cmd): void {
+    $tokens = preg_split("/\s+/", $cmd);
+
+    var_dump($tokens);
+    switch ($favcolor) {
+      case "red":
+        echo "Your favorite color is red!";
+        break;
+      case "blue":
+        echo "Your favorite color is blue!";
+        break;
+      case "green":
+        echo "Your favorite color is green!";
+        break;
+      default:
+        echo "Your favorite color is neither red, blue, nor green!";
+    }
+
+    if ($tokens[0] == 'q') {
+      return;
+    }
   }
 }
 
