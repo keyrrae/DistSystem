a7d64ab78a9e7d2ce37a1c8b56f32dc0f9537b41
keyrrae <xuan-wang@outlook.com>
2017-02-14 23:53:16 -0800

    client

--- TicketBoxRaft/Client/Client.hh
+++ TicketBoxRaft/Client/Client.hh
@@ -1,11 +1,31 @@
 <?hh
 
+class Config {
+  public string $serverAddress;
+  public int $maxAttempt;
+  public int $timeout;
+
+  public function __construct(string $confFilename): void {
+    $jsonStr = file_get_contents($confFilename);
+    $json = json_decode($jsonStr, true);
+    $this->serverAddress = $json['address'];
+    $this->maxAttempt = $json['max_attempts'];
+    $this->timeout = $json['timeout'];
+
+  }
+}
+
 class Client {
+
+  private Config $config;
+
   public function printUsage(): void {
-    echo "b <Number of tickets\n";
+    echo "b <Number of tickets>\n";
   }
 
-  public function __construct(private string $conf) {}
+  public function __construct(string $confFilename) {
+    $this->config = new Config($confFilename);
+  }
 
   public function run(): void {
     $this->printUsage();
